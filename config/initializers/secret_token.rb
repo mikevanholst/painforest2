67e5a3ea35f038e0fc737fdfe03a25b1a2a636a0
Michael van Holst <mikevanholst@gmail.com>
2013-09-11 16:36:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Painforest2::Application.config.secret_key_base = '82189ff09bc37060a12ad345eb637b0e0e92e881a71ce2c8acdccff0f337ff3c0937acb8e15d3b77eabbeefdb4a104ba2eadd290cafce464b1f7accd7115fd3e'
